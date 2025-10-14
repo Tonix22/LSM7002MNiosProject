257595e3c0013a66d128cc575ee62b20919a9b41
Tonix22 <emiliotonix@gmail.com>
2025-10-14 14:31:17 -0600

    Changes clean repo

--- MasterSoC_Controller/software/MasterSoC_3pAIP_v1/obj/default/ID00004003_masterSOC.d
+++
@@ -1,6 +0,0 @@
-obj/default/ID00004003_masterSOC.o: ID00004003_masterSOC.c \
- ID00004003_masterSOC.h aip.h
-
-ID00004003_masterSOC.h:
-
-aip.h:
--- MasterSoC_Controller/software/MasterSoC_3pAIP_v1/obj/default/aip.d
+++
@@ -1,8 +0,0 @@
-obj/default/aip.o: aip.c aip.h ../MasterSoC_3pAIP_v1_bsp/system.h \
- ../MasterSoC_3pAIP_v1_bsp/linker.h
-
-aip.h:
-
-../MasterSoC_3pAIP_v1_bsp/system.h:
-
-../MasterSoC_3pAIP_v1_bsp/linker.h:
